df5d8618adcda449d46e95bb429c70ba070db8fd
Joseph Lee <jlee01@stuy.edu>
2018-10-10 00:27:46 -0400

    created Triangle class

---
+++ Triangle.java
@@ -0,0 +1,12 @@
+public class Triangle{
+  private point1;
+  private point2
+  private point3;
+
+  public Triangle(Point Point1,Point Point2,Point Point3){
+    point1 = new point (Point1);
+    point2 = new point (Point2);
+    point3 = new point (Point3);
+}
+
+  public Triangle()
